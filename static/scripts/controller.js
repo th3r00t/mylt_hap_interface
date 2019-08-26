1dd0885f5960913e0a5442cfaef849b8dd3c3324
Raelon Masters <admin@mylocaltech.co>
2019-08-26 16:56:19 -0400

    Adjusting ajax call to deal with live hosting issue

--- static/scripts/controller.js
+++ static/scripts/controller.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
 
     $("div#benchlight").click(function() {
         console.log('toggle')
-        rt = $.post('/controller') // , $(this).id)
-        console.log(rt.responseText)
+        $.ajax({
+            type: "POST",
+            url: '/controller',
+            data: '',
+            success: '',
+            dataType: 'json'
+        });
+        // rt = $.post('/controller') // , $(this).id)
+        // console.log(rt.responseText)
     });
 });
